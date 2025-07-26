fcd11b654821af8622b568b84bf90264aa7fcde5
priyansh raj <priyansh2005r@gmail.com>
2025-07-27 03:01:21 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Navbar from './components/Navbar/Navbar'
+import Hero from './components/Hero/Hero'
+import Programs from './components/Programs/Programs'
+import Title from './components/Title/Title'
+import About from './components/About/About'
+import Campus from './components/Campus/Campus'
+import Testimonials from './components/Testimonials/Testimonials'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Hero/>
+      <div className="container">
+        <Title subTitle='Our PROGRAM' title='what We Offer' />
+      <Programs/>
+      <About/>
+      <Title subTitle='Gallery' title='Campus photos' />
+      <Campus/>
+      <Title subTitle='Testimonials' title='What Our Students Say' />
+      <Testimonials/>
+      </div>
+    </div>
+  )
+}
+
+export default App
